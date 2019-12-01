129000264f06367891ef1203e69d3310b3f1499d
Abzy <Alexx1103@AOL.com>
2019-12-01 11:54:54 +0100

    Implement GetTotalFuel method

--- AdventOfCode/Day01/FuelCalculator.cs
+++ AdventOfCode/Day01/FuelCalculator.cs
@@ -3,5 +3,15 @@
     public class FuelCalculator
     {
         public int GetFuel(int mass) => (mass / 3) - 2;
+
+        public int GetTotalFuel(int mass)
+        {
+            var fuel = GetFuel(mass);
+            while (fuel > 0)
+            {
+                return fuel + GetTotalFuel(fuel);
+            }
+            return 0;
+        }
     }
 }
