161d96accb44b0892a2a60050d4c074bfe59b4b8
Abzy <Alexx1103@AOL.com>
2019-12-01 08:54:50 +0100

    Init
    - Add test project
    - Add NUnit
    - Add FluentAssertions

---
+++ AdventOfCode.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace AdventOfCode.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
