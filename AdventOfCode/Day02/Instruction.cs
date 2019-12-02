4611e037bf332bcd6f080c52cb2c2f166878cce9
Abzy <Alexx1103@AOL.com>
2019-12-02 22:43:00 +0100

    Add IntcodeParser
    - add Instruction class

---
+++ AdventOfCode/Day02/Instruction.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode.Day02
+{
+    public class Instruction
+    {
+        public int Opcode { get; set; }
+        public int ParameterA { get; set; }
+        public int ParameterB { get; set; }
+        public int DestinationIndex { get; set; }
+    }
+}
