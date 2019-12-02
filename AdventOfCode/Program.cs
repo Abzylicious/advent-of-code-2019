f3f101e7df6abc34869408c5beb8e60463665f61
Abzy <Alexx1103@AOL.com>
2019-12-02 01:22:21 +0100

    Adjust Program class to dynamically select a day

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode.Services;
+using System;
 
 namespace AdventOfCode
 {
@@ -6,7 +7,40 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var solveService = new SolveService();
+            Console.WriteLine("Advent of Code 2019");
+            Console.WriteLine("\nEnter a day to view its solution.");
+
+            do
+            {
+                var day = ReadDay();
+                solveService.GetSolution(day);
+                Console.WriteLine("\nDo you want to check upon other days? Press any key to continue, press 'ESCAPE' to exit.");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
+
+        private static int ReadDay()
+        {
+            var daySelection = ReadInteger();
+            if (!IsCalendarDay(daySelection))
+            {
+                Console.WriteLine("Code of Advent takes place between the 1. and 25. of December. Please select a fitting value.");
+                ReadDay();
+            }
+
+            return daySelection;
+        }
+
+        private static int ReadInteger()
+        {
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("The entered value is not a number.");
+            }
+            return result;
+        }
+
+        private static bool IsCalendarDay(int day) => day >= 0 && day <= 25;
     }
 }
