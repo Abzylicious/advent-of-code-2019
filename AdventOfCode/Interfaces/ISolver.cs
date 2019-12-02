50ef77af9c1ebbbf47ba186822334ba94202a360
Abzy <Alexx1103@AOL.com>
2019-12-02 01:19:26 +0100

    Implement ISolver interface

---
+++ AdventOfCode/Interfaces/ISolver.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode.Interfaces
+{
+    public interface ISolver
+    {
+        public int Day { get; }
+        public string Title { get; }
+        public void Precondition();
+        public string GetFirstSolution();
+        public string GetSecondSolution();
+    }
+}
