a63c63502353880e5cf6e1f2d312475e91e9118a
Abzy <Alexx1103@AOL.com>
2019-12-04 00:59:05 +0100

    Implement GetClosestIntersection method

--- AdventOfCode/Day03/Grid.cs
+++ AdventOfCode/Day03/Grid.cs
@@ -10,5 +10,20 @@ namespace AdventOfCode.Day03
         public Wire WireA { get; set; }
         public Wire WireB { get; set; }
 
+        public IEnumerable<Point> GetIntersections() => WireA.ConnectedPoints.Intersect(WireB.ConnectedPoints);
+
+        public int GetClosestIntersection()
+        {
+            var intersections = GetIntersections();
+            var distances = new List<int>();
+
+            foreach (var intersection in intersections)
+            {
+                distances.Add(CalculateManhattanDistance(intersection));
+            }
+            return distances.Min();
+        }
+
+        private int CalculateManhattanDistance(Point point) => Math.Abs(point.X) + Math.Abs(point.Y);
     }
 }
