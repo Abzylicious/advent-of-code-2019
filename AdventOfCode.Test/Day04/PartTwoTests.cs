2c5fb2f48c1937999eb69c5f544e9a97840429d8
Abzy <Alexx1103@AOL.com>
2019-12-04 22:17:48 +0100

    Add example test cases for part 2

---
+++ AdventOfCode.Test/Day04/PartTwoTests.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.Day04;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Test.Day04
+{
+    [TestFixture]
+    public class PartTwoTests
+    {
+        private readonly PasswordCombiner _passwordCombiner = new PasswordCombiner();
+
+        /// <summary>
+        /// 112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+        /// </summary>
+        [Test]
+        public void ExampleOne()
+        {
+            var actual = _passwordCombiner.IsValid(112233, null);
+            actual.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// 123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+        /// </summary>
+        [Test]
+        public void ExampleTwo()
+        {
+            var actual = _passwordCombiner.IsValid(123444, null);
+            actual.Should().BeFalse();
+        }
+
+        /// <summary>
+        /// 111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+        /// </summary>
+        [Test]
+        public void ExampleThree()
+        {
+            var actual = _passwordCombiner.IsValid(111122, null);
+            actual.Should().BeTrue();
+        }
+    }
+}
