1704246756207b872adfffbd1f1baa0e89b98a42
Abzy <Alexx1103@AOL.com>
2019-12-04 21:49:45 +0100

    Add PasswordCombiner class
    - add IPasswordRule interface

---
+++ AdventOfCode/Day04/PasswordCombiner.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode.Interfaces;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Day04
+{
+    public class PasswordCombiner
+    {
+
+        public bool IsValid(int number, params IPasswordRule[] ruleset) => IsValid(GetDigits(number), ruleset);
+
+        private bool IsValid(List<int> digits, params IPasswordRule[] rulesets)
+        {
+            foreach (var rule in rulesets)
+            {
+                if (!rule.Check(digits))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private List<int> GetDigits(int number)
+        {
+            var digits = new List<int>();
+            while (number > 0)
+            {
+                digits.Add(number % 10);
+                number /= 10;
+            }
+
+            digits.Reverse();
+            return digits;
+        }
+    }
+}
---
+++ AdventOfCode/Interfaces/IPasswordRule.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Interfaces
+{
+    public interface IPasswordRule
+    {
+        bool Check(List<int> digits);
+    }
+}
