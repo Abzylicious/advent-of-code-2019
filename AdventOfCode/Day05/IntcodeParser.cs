77d4faedd8136c743171730701245aba9a0ec68e
Abzy <Alexx1103@AOL.com>
2019-12-05 17:45:39 +0100

    Add IntcodeParser (Day 05)

---
+++ AdventOfCode/Day05/IntcodeParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Day05
+{
+    public class IntcodeParser
+    {
+        private int[] _memory;
+
+        public int[] Parse(List<int> intcode)
+        {
+            _memory = intcode.ToArray();
+            return null;
+        }
+    }
+}
