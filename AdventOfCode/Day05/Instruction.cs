3a061b80ae3b08203324fcdc7025035f302be6ba
Abzy <Alexx1103@AOL.com>
2019-12-05 17:56:57 +0100

    Add Instruction model

---
+++ AdventOfCode/Day05/Instruction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day05
+{
+    public class Instruction
+    {
+        public int Opcode { get; set; }
+        public List<int> Parameters { get; set; }
+
+        public Instruction()
+        {
+            Parameters = new List<int>();
+        }
+
+        public int ParameterCount() => Parameters.Count + 1;
+    }
+}
