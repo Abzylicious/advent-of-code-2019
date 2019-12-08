41307e0b3777f87e9167530741f785d54b3a1e5f
Abzy <Alexx1103@AOL.com>
2019-12-08 20:05:50 +0100

    Add example test cases for part 1

---
+++ AdventOfCode.Test/Day08/PartOneTests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Day08;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode.Test.Day08
+{
+    [TestFixture]
+    public class PartOneTests
+    {
+        private readonly ImageDecoder _imageDecoder = new ImageDecoder();
+
+        [Test]
+        public void ExampleOne()
+        {
+            var imageData = "123456789012";
+            var image = _imageDecoder.CreateNewSpaceImage(imageData, 3, 2);
+            var actual = _imageDecoder.GetChecksum(image);
+            actual.Should().Be(1);
+        }
+    }
+}
