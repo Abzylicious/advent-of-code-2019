d7bbd9ff02eb59333a72cf54958e24e3582c1341
Abzy <Alexx1103@AOL.com>
2019-12-08 20:04:34 +0100

    Add models for Day08
    - add SpaceImage model
    - add SpaceImageLayer model

---
+++ AdventOfCode/Day08/SpaceImage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day08
+{
+    public class SpaceImage
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public List<SpaceImageLayer> Layers { get; set; } = new List<SpaceImageLayer>();
+    }
+}
---
+++ AdventOfCode/Day08/SpaceImageLayer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day08
+{
+    public class SpaceImageLayer
+    {
+        public List<string> Rows { get; set; } = new List<string>();
+    }
+}
