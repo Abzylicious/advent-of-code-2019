61447cd430accc14a18d97c3051a36fa409d4b0f
Abzy <Alexx1103@AOL.com>
2019-12-09 21:52:44 +0100

    Add Boost class

---
+++ AdventOfCode/Day09/Boost.cs
@@ -0,0 +1,75 @@
+﻿using AdventOfCode.Computer;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Day09
+{
+    public class Boost : IntcodeParser
+    {
+        private int _relativeBase;
+        private int _input;
+        private readonly List<long> _output = new List<long>();
+
+        public List<long> Parse(List<long> intcode, int input)
+        {
+            _input = input;
+            return Parse(intcode);
+        }
+
+        public List<long> Parse(List<long> intcode)
+        {
+            _output.Clear();
+            _memory = intcode;
+            Run();
+            return _output;
+        }
+
+        protected override int ExecuteInstruction(Instruction instruction, int instructionPointer)
+        {
+            var nextInstructionPointer = instructionPointer + instruction.ParameterCount();
+            switch (instruction.Opcode)
+            {
+                case Opcode.WRITE:
+                    SetMemoryAt((int)instruction.Parameters[0], _input);
+                    return nextInstructionPointer;
+                case Opcode.OUTPUT:
+                    _output.Add(instruction.Parameters[0]);
+                    return nextInstructionPointer;
+                case Opcode.MODIFY_OFFSET:
+                    _relativeBase += (int)instruction.Parameters[0];
+                    return nextInstructionPointer;
+
+            }
+            return base.ExecuteInstruction(instruction, instructionPointer);
+        }
+
+        protected override List<long> GetParameters(Opcode opcode, int instructionPointer)
+        {
+            var opcodeInstruction = _memory[instructionPointer];
+            var parameters = new List<long>();
+
+            switch (opcode)
+            {
+                case Opcode.WRITE:
+                    parameters.Add(GetValue(instructionPointer + 1, ParameterMode.IMMEDIATE));
+                    break;
+                case Opcode.OUTPUT:
+                case Opcode.MODIFY_OFFSET:
+                    parameters.Add(GetValue(instructionPointer + 1, GetMode(opcodeInstruction, 1)));
+                    break;
+                default:
+                    return base.GetParameters(opcode, instructionPointer);
+            }
+
+            return parameters;
+        }
+
+        protected override long GetValue(int index, ParameterMode mode) => mode switch
+        {
+            ParameterMode.POSITIONAL => (int)_memory[index] > _memory.Count - 1 ? 0 : _memory[(int)_memory[index]],
+            ParameterMode.IMMEDIATE => index > _memory.Count - 1 ? 0 : _memory[index],
+            ParameterMode.RELATIVE => ((int)_memory[index] + _relativeBase) > _memory.Count - 1 ? 0 : _memory[(int)_memory[index] + _relativeBase],
+            _ => throw new Exception("Unknown parameter mode detected.")
+        };
+    }
+}
