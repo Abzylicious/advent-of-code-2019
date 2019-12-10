22f87202f19f505fc8db4b0c8dbd02c5b7de1d6c
Abzy <Alexx1103@AOL.com>
2019-12-10 04:20:46 +0100

    Boost rewrite

--- AdventOfCode/Day09/Boost.cs
+++ AdventOfCode/Day09/Boost.cs
@@ -4,73 +4,119 @@ using System.Collections.Generic;
 
 namespace AdventOfCode.Day09
 {
-    public class Boost : IntcodeParser
+    public class Boost
     {
+        private List<long> _memory = new List<long>();
+        private int _input;
+        private List<long> _output = new List<long>();
         private int _relativeBase;
-        private List<int> _inputs = new List<int>();
-        private readonly List<long> _output = new List<long>();
+        private int _instructionPointer;
+        private int[] modeMask = new int[] { 0, 100, 1000, 10000 };
+
 
         public List<long> Parse(List<long> intcode, int input)
         {
-            _inputs.Clear();
-            _inputs.Add(input);
+            _input = input;
             return Parse(intcode);
         }
 
         public List<long> Parse(List<long> intcode)
         {
             _output.Clear();
-            _relativeBase = 0;
             _memory = intcode;
+            _relativeBase = 0;
+            _instructionPointer = 0;
             Run();
             return _output;
         }
 
-        protected override int ExecuteInstruction(Instruction instruction, int instructionPointer)
+        public void Run()
         {
-            var nextInstructionPointer = instructionPointer + instruction.ParameterCount();
-            switch (instruction.Opcode)
+            while ((Opcode)_memory[_instructionPointer] != Opcode.END)
             {
-                case Opcode.WRITE:
-                    SetMemoryAt((int)instruction.Parameters[0], _inputs[0]);
-                    _inputs.RemoveAt(0);
-                    return nextInstructionPointer;
-                case Opcode.OUTPUT:
-                    _output.Add(instruction.Parameters[0]);
-                    return nextInstructionPointer;
-                case Opcode.MODIFY_OFFSET:
-                    _relativeBase += (int)instruction.Parameters[0];
-                    return nextInstructionPointer;
-
+                var opcode = (Opcode)(_memory[_instructionPointer] % 100);
+                ExecuteInstruction(opcode);
             }
-            return base.ExecuteInstruction(instruction, instructionPointer);
         }
 
-        protected override List<long> GetParameters(Opcode opcode, int instructionPointer)
+        private void ExecuteInstruction(Opcode opcode)
         {
-            var opcodeInstruction = _memory[instructionPointer];
-            var parameters = new List<long>();
-
             switch (opcode)
             {
+                case Opcode.ADD:
+                    _memory[(int)GetAddress(3)] = GetParameter(1) + GetParameter(2);
+                    _instructionPointer += 4;
+                    break;
+                case Opcode.MULTIPLY:
+                    _memory[(int)GetAddress(3)] = GetParameter(1) * GetParameter(2);
+                    _instructionPointer += 4;
+                    break;
                 case Opcode.WRITE:
+                    _memory[(int)GetAddress(1)] = _input;
+                    _instructionPointer += 2;
+                    break;
                 case Opcode.OUTPUT:
+                    _output.Add(GetParameter(1));
+                    _instructionPointer += 2;
+                    break;
+                case Opcode.JUMP_IF_TRUE:
+                    _instructionPointer = GetParameter(1) != 0 ? (int)GetParameter(2) : _instructionPointer + 3;
+                    break;
+                case Opcode.JUMP_IF_FALSE:
+                    _instructionPointer = GetParameter(1) == 0 ? (int)GetParameter(2) : _instructionPointer + 3;
+                    break;
+                case Opcode.LESS_THAN:
+                    _memory[(int)GetAddress(3)] = GetParameter(1) < GetParameter(2) ? 1 : 0;
+                    _instructionPointer += 4;
+                    break;
+                case Opcode.EQUALS:
+                    _memory[(int)GetAddress(3)] = GetParameter(1) == GetParameter(2) ? 1 : 0;
+                    _instructionPointer += 4;
+                    break;
                 case Opcode.MODIFY_OFFSET:
-                    parameters.Add(GetValue(instructionPointer + 1, GetMode(opcodeInstruction, 1)));
+                    _relativeBase += (int)GetParameter(1);
+                    _instructionPointer += 2;
                     break;
                 default:
-                    return base.GetParameters(opcode, instructionPointer);
+                    throw new Exception($"Opcode {opcode} failed to execute.");
             }
-
-            return parameters;
         }
 
-        protected override long GetValue(int index, ParameterMode mode) => mode switch
+        private long GetParameter(int relativeMemoryIndex) => _memory[(int)GetAddress(relativeMemoryIndex)];
+
+        private long GetAddress(int relativeMemoryIndex)
         {
-            ParameterMode.POSITIONAL => (int)_memory[index] > _memory.Count - 1 ? 0 : _memory[(int)_memory[index]],
-            ParameterMode.IMMEDIATE => _memory[index],
-            ParameterMode.RELATIVE => _memory[(int)_memory[index] + _relativeBase],
-            _ => throw new Exception("Unknown parameter mode detected.")
-        };
+            var mode = (ParameterMode)(_memory[_instructionPointer] / modeMask[relativeMemoryIndex] % 10);
+            switch (mode)
+            {
+                case ParameterMode.POSITIONAL:
+                    ExtendMemoryIfNeeded((int)_memory[_instructionPointer + relativeMemoryIndex]);
+                    return _memory[_instructionPointer + relativeMemoryIndex];
+                case ParameterMode.IMMEDIATE:
+                    ExtendMemoryIfNeeded(_instructionPointer + relativeMemoryIndex);
+                    return _instructionPointer + relativeMemoryIndex;
+                case ParameterMode.RELATIVE:
+                    ExtendMemoryIfNeeded(_relativeBase + (int)_memory[_instructionPointer + relativeMemoryIndex]);
+                    return _relativeBase + _memory[_instructionPointer + relativeMemoryIndex];
+                default:
+                    throw new Exception("Unknown parameter mode detected.");
+            }
+        }
+
+        private void ExtendMemoryIfNeeded(int accessedIndex)
+        {
+            if (accessedIndex > _memory.Count - 1)
+            {
+                ExtendMemory(accessedIndex - _memory.Count + 1);
+            }
+        }
+
+        private void ExtendMemory(int elements)
+        {
+            for (int i = 0; i < elements; i++)
+            {
+                _memory.Add(0);
+            }
+        }
     }
 }
