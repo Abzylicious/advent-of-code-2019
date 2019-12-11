e770dc86ec7cd4dccc2f38e139e7438462b910b6
Abzy <Alexx1103@AOL.com>
2019-12-11 20:19:42 +0100

    Add AsteroidMap

---
+++ AdventOfCode/Day10/AsteroidMap.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace AdventOfCode.Day10
+{
+    public class AsteroidMap
+    {
+        private readonly IDictionary<Point, char> _asteroidMap;
+
+        public AsteroidMap()
+        {
+            _asteroidMap = new Dictionary<Point, char>();
+        }
+
+        public AsteroidMap(string[] asteroidMapData) : this()
+        {
+            CreateNew(asteroidMapData);
+        }
+
+        public void CreateNew(string[] asteroidMapData)
+        {
+            _asteroidMap.Clear();
+            var xAxisLength = asteroidMapData[0].Length;
+            var yAxisLength = asteroidMapData.Length;
+
+            for (int y = 0; y < yAxisLength; y++)
+            {
+                for (int x = 0; x < xAxisLength; x++)
+                {
+                    var nextPoint = new Point(x, y);
+                    var value = Convert.ToChar(asteroidMapData[y].Substring(x, 1));
+                    _asteroidMap.Add(nextPoint, value);
+                }
+            }
+        }
+
+        public (Point position, int detectedAsteroids) GetBestScanStationPosition()
+        {
+            var result = GetAsteroidDetectedMap().OrderByDescending(x => x.Value).FirstOrDefault();
+            return (result.Key, result.Value);
+        }
+
+        private IDictionary<Point, int> GetAsteroidDetectedMap()
+        {
+            var result = new Dictionary<Point, int>();
+            foreach (var position in _asteroidMap.Where(x => x.Value == '#'))
+            {
+                result.Add(position.Key, GetAsteroidsDetected(position.Key));
+            }
+
+            return result;
+        }
+
+        private int GetAsteroidsDetected(Point currentPosition)
+        {
+            var angles = new List<double>();
+            foreach (var position in _asteroidMap)
+            {
+                if (position.Key != currentPosition && position.Value == '#')
+                {
+                    angles.Add(CalculateAngle(currentPosition, position.Key));
+                }
+            }
+
+            return angles.GroupBy(d => d).Count();
+        }
+
+        private double CalculateAngle(Point a, Point b)
+        {
+            var deltaX = b.X - a.X;
+            var deltaY = b.Y - a.Y;
+            return Math.Atan2(deltaY, deltaX) * 180 / Math.PI;
+        }
+    }
+}
